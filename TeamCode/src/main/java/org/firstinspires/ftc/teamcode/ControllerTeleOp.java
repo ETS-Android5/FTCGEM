bf219c2042cd2af7eba4e8d8c72f7b47f1af7bb8
IK_Knight <IK_Knight@outlook.com>
2022-03-25 13:19:27 -0500

    Registered ControllerTeleOp.java V0.0.6

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ControllerTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ControllerTeleOp.java
@@ -8,7 +8,12 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
+
+
 @TeleOp(name = "Concept: ControllerTeleOp", group = "ControllerTeleOp")
+
+
+
 public class ControllerTeleOp extends OpMode {
     final static double CLAW_MIN_RANGE = 0.0;
     final static double CLAW_MAX_RANGE = 1.0;
